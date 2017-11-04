41e663dcfc77789eddb0843884968866e6d1aab0
gokturkenez <hi@gokturkenez.com.tr>
2017-11-04 23:38:04 +0300

    Release version

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# PayU_TR_IPN_API_PythonSample
-PayU Turkey IPN API Python Sample Code
+# PayU Turkey IPN API Python3 Sample 
+    
+    In this sample code Python 3 and Flask used.
+    
+ # PayU Turkey IPN API Python 3 Sample Code v1.0
+    
+    * This is an **unofficial** sample code.
+    * This version is a release version, there is can be some fix needs, just let me know if you face 
+    with something wrong.
+    * This sample code developed on Python 3.4.2 and Flask
+    * Below libraries used when developing
+         collections
+         flask
+         datetime > datetime
+         hmac
+         hashlib
+    * We are not owner any used libraries, all rights reserved to the owners.
+    * Datetime can be changed due to your system. 
+    
+# Requirements
+
+flask should be installed and imported for creating and running app for handling IPN Post request.
+You can follow this [instructor](http://flask.pocoo.org/docs/0.12/installation/)  for installing flask.
+
+ 
---
+++ ipn.py
@@ -0,0 +1,49 @@
+'''
+Project: PayU Turkey IPN Python Sample
+Author: Gokturk Enez
+'''
+import collections
+import flask
+from datetime import datetime
+import hmac
+import hashlib
+
+ipn = flask.Flask(__name__)
+date = datetime.utcnow().strftime('%Y%m%d%H%M%S')
+# Your Merchant's Secret key
+secretkey = "SECRET_KEY"
+
+
+# You can change IPN URL with editing "/ipn"
+@ipn.route("/ipn", methods=("POST", "GET"))
+def webhook_post_handler():
+    # You can reach IPN requets's all @params with ipnparams
+    ipnparams = flask.request.values
+    print(ipnparams)
+
+    # Creating array for hash calculation
+    array = collections.OrderedDict()
+    array['IPN_PID'] = ipnparams["IPN_PID[]"]
+    array['IPN_PNAME'] = ipnparams["IPN_PNAME[]"]
+    array['IPN_DATE'] = ipnparams["IPN_DATE"]
+    array['DATE'] = date
+
+    # Initializing the hashstring @param
+    hashstring = ''
+
+    for k, v in array.items():
+        # Adding the UTF-8 byte length of each field value at the beginning of field value
+        hashstring += str(len(v)) + str(v)
+    print(hashstring)
+
+    # Signature Calculation
+    signature = hmac.new(secretkey.encode('utf-8'), hashstring.encode('utf-8'), hashlib.md5).hexdigest()
+    print(signature)
+
+    # Printing response
+    return flask.Response("<EPAYMENT>{0}|{1}</EPAYMENT>".format(date, signature))
+
+
+if __name__ == '__main__':
+    # You can edit or delete port and debug mode.
+    ipn.run(port=8080, debug=True)
